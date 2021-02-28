adaf9b0dcac2c8161cd9d77e569cc597c24420f8
Daniel Windhede <daniel.windhede@gmail.com>
2021-02-28 20:42:55 +0100

    Extension button

---
+++ popup/popup.js
@@ -0,0 +1,28 @@
+
+const hidePage = `body > :not(.beastify-image) {
+                    display: none;
+                  }`;
+
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+
+    });
+}
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute content script: ${error.message}`);
+}
+
+browser.tabs.executeScript({ file: "/extractor.js" })
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
